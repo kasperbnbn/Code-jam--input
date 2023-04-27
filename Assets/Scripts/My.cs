ba429928b0f2ab6134ae9907b433c041d8d63ec1
kasperbnbn <kasperbnbn@live.dk>
2023-04-27 10:37:48 +0200

    Update My.cs

--- Assets/Scripts/My.cs
+++ Assets/Scripts/My.cs
@@ -13,9 +13,12 @@ public class My : MonoBehaviour
 
     void Start()
     {
+        //https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager-sceneLoaded.html
         previousSceneName = SceneManager.GetActiveScene().name;
         SceneManager.sceneLoaded -= OnSceneLoaded; // Unsubscribe first
         SceneManager.sceneLoaded += OnSceneLoaded; // Subscribe to the sceneLoaded event
+       
+        //https://forum.unity.com/threads/using-only-one-eventsystem.1330017/
         DontDestroyOnLoad(gameObject);
     }
 
@@ -23,31 +26,38 @@ public class My : MonoBehaviour
     {
         Debug.Log("Scene loaded: " + scene.name);
 
+        if(puzzleDestroyed)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("puzle"));
+        }
+
+        if (puzleDestroyed)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("puzzle"));
+        }
+
         if (previousSceneName == "Scene1" && scene.name == "Scene" && !puzleDestroyed)
         {
             Debug.Log("Transitioning from Scene 1 to Scene");
             // Do something when going from Scene 1 to Scene 2
-            GameObject puzle = GameObject.FindGameObjectWithTag("puzle");
-            if (puzle != null)
-            {
-                Destroy(puzle);
-                puzleDestroyed = true;
+           Destroy(GameObject.FindGameObjectWithTag("puzle"));
+            puzleDestroyed = true;
+
                 Debug.Log("Destroyed puzle");
-            }
+         
         }
         else if (previousSceneName == "Scene2" && scene.name == "Scene" && !puzzleDestroyed)
         {
             Debug.Log("Transitioning from Scene 2 to Scene");
             // Do something when going from Scene 3 to Scene 4
-            GameObject puzzle = GameObject.FindGameObjectWithTag("puzzle");
-            if (puzzle != null)
-            {
-                Destroy(puzzle);
-                puzzleDestroyed = true;
-                Debug.Log("Destroyed puzzle");
-            }
+          Destroy(GameObject.FindGameObjectWithTag("puzzle"));
+            puzzleDestroyed = true;
+          
+           
+            Debug.Log("Destroyed puzzle");
         }
 
         previousSceneName = scene.name; // Update the previous scene name
     }
+  
 }
